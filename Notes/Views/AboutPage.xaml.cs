d3d7dc829b64145f122910ddaee301f8152639e7
Angell50 <angel.vela@udla.edu.ec>
2024-12-22 22:07:29 -0500

    creacion,actualizacion y limpieza de codigo

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -5,18 +5,5 @@ public partial class AboutPage : ContentPage
 	public AboutPage()
 	{
 		InitializeComponent();
-
-        BindingContext = new Models.About
-        {
-            MoreInfoUrl = "https://www.youtube.com/watch?v=20GwG0dcAsA"
-        };
-    }
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.About about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
     }
 }
